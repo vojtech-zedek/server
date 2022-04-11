0e0093dbb47922f6a503120a4e93e4ff3f4d9a6b
vojtech-zedek <vojtazed@seznam.cz>
2022-04-11 02:01:19 +0200

    02 - Form

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000.');
 });
+
+app.use(express.static('public'));
